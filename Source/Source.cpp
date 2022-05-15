d70c19a2d7a2690770d44b0952a91f2539debf53
Ben Russell <benalvinrussell@gmail.com>
2022-05-15 02:57:55 -0400

    update for mainmenu options 1-3

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -17,7 +17,10 @@ void AddToCustomer(sqlite3* db);
 void AddToProduct(sqlite3* db);
 void AddToMusic(sqlite3* db);
 void AddToInstrument(sqlite3* db);
+void UpdateTable(sqlite3* db, int u);
+void DeleteTableEntry(sqlite3*, int d);
 sqlite3_stmt* RunQuery(sqlite3* db, string q);
+vector<string> MakeVector(sqlite3* db, string& q, sqlite3_stmt* s);
 bool Finalize(sqlite3* db, string q);
 bool CheckIfValidCurrency(string const& input);
 
@@ -43,7 +46,7 @@ int main()
 		try { choice = stoi(uin); }
 		catch (...) { choice = 0; }
 		switch (choice) {
-		case 1:
+		case 1: // Add to table
 			PrintMenu(1);
 			while (1) {
 				getline(cin, uin);
@@ -64,7 +67,19 @@ int main()
 				else { cout << "Invalid choice. Please try again.\n"; }
 			}
 			break;
-		case 2:
+		case 2: // Update table
+			PrintMenu(2);
+			while (1) {
+				getline(cin, uin);
+				if (uin == "-1") { break; }
+				try { num = stoi(uin); }
+				catch (...) { num = 0; }
+				if (num > 0 && num <= 9) {
+					UpdateTable(mydb, num);
+					break;
+				}	
+				else { cout << "Invalid choice. Please try again.\n"; }
+			}
 			break;
 		case 3:
 			break;
@@ -86,6 +101,7 @@ int main()
 }
 
 void PrintMenu(int c) {
+	string tables[9] = { "1. Store", "2. Employee", "3. Customer", "4. Product", "5. Music", "6. Instrument", "7. Sales", "8. Rentals", "9. Services" };
 	switch (c) {
 	case 0: // Main menu.
 		cout << "1. Add data to table.\n";
@@ -99,13 +115,15 @@ void PrintMenu(int c) {
 	case 1: // Add to table.
 		cout << "Please select which table you would like to add data to. Type -1 to cancel.\n";
 		cout << "NOTE: Tables not listed can be added to elsewhere.\n";
-		cout << "1. Store\n";
-		cout << "2. Employee\n";
-		cout << "3. Customer\n";
-		cout << "4. Product\n";
-		cout << "5. Music\n";
-		cout << "6. Instrument\n";
+		for (int i = 0; i <= 5; ++i) { cout << tables[i] << endl; }
 		break;
+	case 2: // Update table.
+		cout << "Please select which table you would like to update the data of. Type -1 to cancel.\n";
+		for (string s : tables) { cout << s << endl; }
+		break;
+	case 3: // Delete data from table.
+		cout << "Please select which table you would like to delete data from. Type -1 to cancel.\n";
+		for (string s : tables) { cout << s << endl; }
 	}
 
 	
@@ -146,14 +164,9 @@ void AddToEmployee(sqlite3* db) {
 
 	string query = "SELECT STORE_ID FROM STORE";
 	sqlite3_stmt* ate = RunQuery(db, query);
-	if (ate != nullptr) {
-		while (sqlite3_step(ate) == SQLITE_ROW) {
-			stores.push_back(reinterpret_cast<const char*>(sqlite3_column_text(ate, 0)));
-		}
-	}
-	else { return; }
+	stores = MakeVector(db, query, ate);
+	if (stores.size() == 0) { return; }
 	sqlite3_finalize(ate);
-	query.clear();
 
 	cout << "Creating a new employee. At any point you may type -1 to cancel.\n";
 	cout << "Please type the new employee's first name.\n";
@@ -197,19 +210,14 @@ void AddToEmployee(sqlite3* db) {
 void AddToCustomer(sqlite3* db) {
 	string cfname, clname, cadd, cphone, ctype;
 	bool hasadd;
-	vector<string> types;
+	vector<string> custtypes;
 
 	// Get all available customer types (FK). 
 	string query = "SELECT CUSTOMER_TYPE FROM CUST_TYPE";
 	sqlite3_stmt* atc = RunQuery(db, query);
-	if (atc != nullptr) {
-		while (sqlite3_step(atc) == SQLITE_ROW) {
-			types.push_back(reinterpret_cast<const char*>(sqlite3_column_text(atc, 0)));
-		}
-	}
-	else { return; }
+	custtypes = MakeVector(db, query, atc);
+	if (custtypes.size() == 0) { return; }
 	sqlite3_finalize(atc);
-	query.clear();
 
 	cout << "Creating a new customer. At any point you may type -1 to cancel.\n";
 	cout << "Please type the new customer's first name.\n";
@@ -227,14 +235,15 @@ void AddToCustomer(sqlite3* db) {
 	getline(cin, cphone);
 	if (cphone == "-1") { return; }
 	cout << "Please indicate the customer's type by selecting a number from the following list:\n";
-	for (int i = 0; i < types.size(); ++i) { cout << i + 1 << ". " << types[i] << endl; }
+	for (int i = 0; i < custtypes.size(); ++i) { cout << i + 1 << ". " << custtypes[i] << endl; }
 	int num;
 	while (1) {
 		getline(cin, ctype);
+		if (ctype == "-1") { return; }
 		try { num = stoi(ctype); }
 		catch (...) { num = -1; }
-		if (num > 0 && num <= types.size()) { 
-			ctype = types[num - 1]; 
+		if (num > 0 && num <= custtypes.size()) { 
+			ctype = custtypes[num - 1]; 
 			break;
 		}
 		else { cout << "Invalid choice. Please try again.\n"; }
@@ -273,6 +282,7 @@ void AddToProduct(sqlite3* db) {
 	int num;
 	while (1) {
 		getline(cin, pstock);
+		if (pstock == "-1") { return; }
 		try { num = stoi(pstock); }
 		catch (...) { num = -1; }
 		if (num >= 0) { break; }
@@ -283,20 +293,303 @@ void AddToProduct(sqlite3* db) {
 	sqlite3_exec(db, "BEGIN;", NULL, NULL, NULL);
 	cout << "Creating product. . .\n";
 	string query = "INSERT INTO PRODUCT (PRODUCT_NAME, PRODUCT_PRICE, PRODUCT_BRAND, PRODUCT_STOCK) ";
-	query += "VALUES ('" + pname + "', ";
-	query += pprice + ", ";
-	query += "'" + pbrand + "', ";
-	query += pstock + ");";
+	query += "VALUES ('" + pname + "', "; // PRODUCT_NAME
+	query += pprice + ", "; // PRODUCT_PRICE
+	query += "'" + pbrand + "', "; // PRODUCT_BRAND
+	query += pstock + ");"; // PRODUCT_STOCK
 	Finalize(db, query);
 }
 
 void AddToMusic(sqlite3* db) {
 	string mname, mprice, mstock, martist, mgenre, mrelease, mtype;
+	vector<string> mustypes;
 
+	// Get available music types (FK)
+	string query = "SELECT MUSIC_TYPE FROM MUS_TYPE";
+	sqlite3_stmt* atm = RunQuery(db, query);
+	mustypes = MakeVector(db, query, atm);
+	if (mustypes.size() == 0) { return; }
+	sqlite3_finalize(atm);
+
+	cout << "Creating a new music product. At any point you may type -1 to cancel.\n";
+	cout << "Please type the new music product's name.\n";
+	getline(cin, mname);
+	if (mname == "-1") { return; }
+	cout << "Please type the new music product's artist/composer.\n";
+	getline(cin, martist);
+	if (martist == "-1") { return; }
+	cout << "Please type the new music product's genre.\n";
+	getline(cin, mgenre);
+	if (mgenre == "-1") { return; }
+	cout << "Please type the new music product's release date.\n";
+	getline(cin, mrelease);
+	if (mrelease == "-1") { return; }
+	cout << "Please type the new music product's price. The currency symbol is not necessary.\n";
+	while (1) {
+		getline(cin, mprice);
+		if (mprice == "-1") { return; }
+		if (CheckIfValidCurrency(mprice)) { break; }
+		else { cout << "Invalid price. Please try again.\n"; }
+	}
+	cout << "Please type the new music product's stock levels.\n";
+	int num;
+	while (1) {
+		getline(cin, mstock);
+		if (mstock == "-1") { return; }
+		try { num = stoi(mstock); }
+		catch (...) { num = -1; }
+		if (num >= 0) { break; }
+		else { cout << "Invalid number. Please try again.\n"; }
+	}
+	cout << "Please indicate the music's type by selecting a number from the following list:\n";
+	for (int i = 0; i < mustypes.size(); ++i) { cout << i + 1 << ". " << mustypes[i] << endl; }
+	while (1) {
+		getline(cin, mtype);
+		if (mtype == "-1") { return; }
+		try { num = stoi(mtype); }
+		catch (...) { num = -1; }
+		if (num > 0 && num <= mustypes.size()) {
+			mtype = mustypes[num - 1];
+			break;
+		}
+		else { cout << "Invalid choice. Please try again.\n"; }
+	}
+
+	cout << "Starting Music add process.\n";
+	sqlite3_exec(db, "BEGIN;", NULL, NULL, NULL);
+	cout << "Creating music. . .\n";
+	query = "INSERT INTO MUSIC (PRODUCT_NAME, PRODUCT_PRICE, PRODUCT_STOCK, MUSIC_ARTIST, MUSIC_GENRE, MUSIC_RELEASEDATE, MUSIC_TYPE) ";
+	query += "VALUES ('" + mname + "', "; // PRODUCT_NAME
+	query += mprice + ", "; // PRODUCT_PRICE
+	query += mstock + ", "; // PRODUCT_STOCK
+	query += "'" + martist + "', "; // MUSIC_ARTIST
+	query += "'" + mgenre + "', "; // MUSIC_GENRE
+	query += "'" + mrelease + "', "; // MUSIC_RELEASEDATE
+	query += "'" + mtype + "');"; // MUSIC_TYPE
+	Finalize(db, query);
 }
 
 void AddToInstrument(sqlite3* db) {
+	string iname, iprice, ibrand, istock, itype, icond;
+	vector<string> insttypes, instconds;
 
+	// Get available instrument conditions (FK)
+	string query = "SELECT INSTRUMENT_CONDITION FROM CONDITION";
+	sqlite3_stmt* ati = RunQuery(db, query);
+	instconds = MakeVector(db, query, ati);
+	if (instconds.size() == 0) { return; }
+
+	// Get available instrument types (FK)
+	query = "SELECT INSTRUMENT_TYPE FROM INST_TYPE";
+	ati = RunQuery(db, query);
+	insttypes = MakeVector(db, query, ati);
+	if (insttypes.size() == 0) { return; }
+	sqlite3_finalize(ati);
+
+	cout << "Creating a new instrument. At any point you may type -1 to cancel.\n";
+	cout << "Please type the new instrument's name.\n";
+	getline(cin, iname);
+	if (iname == "-1") { return; }
+	cout << "Please type the new instrument's brand.\n";
+	getline(cin, ibrand);
+	if (ibrand == "-1") { return; }
+	cout << "Please type the new instrument's price. The currency symbol is not necessary.\n";
+	while (1) {
+		getline(cin, iprice);
+		if (iprice == "-1") { return; }
+		if (CheckIfValidCurrency(iprice)) { break; }
+		else { cout << "Invalid price. Please try again.\n"; }
+	}
+	cout << "Please type the new music product's stock levels.\n";
+	int num;
+	while (1) {
+		getline(cin, istock);
+		if (istock == "-1") { return; }
+		try { num = stoi(istock); }
+		catch (...) { num = -1; }
+		if (num >= 0) { break; }
+		else { cout << "Invalid number. Please try again.\n"; }
+	}
+	cout << "Please indicate the instrument's type by selecting a number from the following list:\n";
+	for (int i = 0; i < insttypes.size(); ++i) { cout << i + 1 << ". " << insttypes[i] << endl; }
+	while (1) {
+		getline(cin, itype);
+		if (itype == "-1") { return; }
+		try { num = stoi(itype); }
+		catch (...) { num = -1; }
+		if (num > 0 && num <= insttypes.size()) {
+			itype = insttypes[num - 1];
+			break;
+		}
+		else { cout << "Invalid choice. Please try again.\n"; }
+	}
+	cout << "Please indicate the instrument's condition by selecting a number from the following list:\n";
+	for (int i = 0; i < instconds.size(); ++i) { cout << i + 1 << ". " << instconds[i] << endl; }
+	while (1) {
+		getline(cin, icond);
+		if (icond == "-1") { return; }
+		try { num = stoi(icond); }
+		catch (...) { num = -1; }
+		if (num > 0 && num <= instconds.size()) {
+			icond = instconds[num - 1];
+			break;
+		}
+		else { cout << "Invalid choice. Please try again.\n"; }
+	}
+
+	cout << "Starting Instrument add process.\n";
+	sqlite3_exec(db, "BEGIN;", NULL, NULL, NULL);
+	cout << "Creating instrument. . .\n";
+	query = "INSERT INTO INSTRUMENT (PRODUCT_NAME, PRODUCT_PRICE, PRODUCT_BRAND, PRODUCT_STOCK, INSTRUMENT_TYPE, INSTRUMENT_CONDITION) ";
+	query += "VALUES ('" + iname + "', "; // PRODUCT_NAME
+	query += iprice + ", "; // PRODUCT_PRICE
+	query += "'" + ibrand + "', "; // PRODUCT_BRAND
+	query += istock + ", "; // PRODUCT_STOCK
+	query += "'" + itype + "', "; // INSTRUMENT_TYPE
+	query += "'" + icond + "');"; // INSTRUMENT_CONDITION
+	Finalize(db, query);
+}
+
+void UpdateTable(sqlite3* db, int u)
+{
+	string what, pkin, uin, update, pk; // what = the table being edited, pkin = the id of the table being edited, pk = name of that table's primary key
+	vector<string> pks; // Collection of valid pks from table.
+	vector<string> atts; // Attributes that can be edited.
+
+	// Switch statement prepares table-specific terminology for sql statement. 
+	switch (u) {
+	case 1: // Store
+		what = "STORE";
+		atts = { "STORE_NAME", "STORE_EMAIL", "STORE_PHONE", "STORE_ADDRESS" };
+		pk = "STORE_ID";
+		break;
+	case 2: // Employee
+		what = "EMPLOYEE";
+		atts = { "STORE_ID", "EMPLOYEE_FIRSTNAME", "EMPLOYEE_LASTNAME", "EMPLOYEE_EMAIL" ,"EMPLOYEE_PHONE" };
+		pk = "EMPLOYEE_ID";
+		break;
+	case 3: // Customer
+		what = "CUSTOMER";
+		atts = { "CUSTOMER_FIRSTNAME", "CUSTOMER_LASTNAME", "CUSTOMER_ADDRESS", "CUSTOMER_PHONE", "CUSTOMER_TYPE" };
+		pk = "CUSTOMER_ID";
+		break;
+	case 4: // Product
+		what = "PRODUCT";
+		atts = { "PRODUCT_NAME", "PRODUCT_PRICE", "PRODUCT_BRAND", "PRODUCT_STOCK" };
+		pk = "PRODUCT_ID";
+		break;
+	case 5: // Music
+		what = "MUSIC";
+		atts = { "PRODUCT_NAME", "PRODUCT_PRICE", "PRODUCT_STOCK", "MUSIC_ARTIST", "MUSIC_GENRE", "MUSIC_RELEASEDATE", "MUSIC_TYPE"};
+		pk = "PRODUCT_ID";
+		break;
+	case 6: // Instrument
+		what = "INSTRUMENT";
+		atts = { "PRODUCT_NAME", "PRODUCT_PRICE", "PRODUCT_BRAND", "PRODUCT_STOCK", "INSTRUMENT_TYPE", "INSTRUMENT_CONDITION"};
+		pk = "PRODUCT_ID";
+		break;
+	case 7: // Sales
+		what = "SALES";
+		atts = { "CUSTOMER_ID", "PRODUCT_ID", "QUANTITY", "PRODUCT_PRICE", "PAYMENT_METHOD", "DISCOUNT" };
+		pk = "ORDER_ID";
+		break;
+	case 8: // Rentals
+		what = "RENTALS";
+		atts = { "CUSTOMER_ID", "PRODUCT_ID", "CHECKOUT_DATE", "CHECKIN_DATE", "MONTHLY_FEE", "PRODUCT_PRICE", "TOTAL_PAID" };
+		pk = "RENTAL_ID";
+		break;
+	case 9: // Services
+		what = "SERVICES";
+		atts = { "SERVICE_DESCRIPTION", "SERVICE_PRICE", "SERVICE_DATE", "CUSTOMER_ID" };
+		pk = "SERVICE_ID";
+		break;
+	}
+
+	string query = "SELECT " + pk + " FROM " + what;
+	sqlite3_stmt* up = RunQuery(db, query);
+	pks = MakeVector(db, query, up);
+	if (pks.size() == 0) { return; }
+
+	cout << "Please type the " + pk + " of the " + what + " you wish to update. Type -1 to cancel.\n";
+	while (1) {
+		getline(cin, pkin);
+		if (pkin == "-1") { return; }
+		if (count(pks.begin(), pks.end(), pkin)) { break; }
+		else { cout << "Invalid " + pk + ". Please try again.\n"; }
+	}
+	cout << "Please indicate which attribute of this " + what + " you wish to update.\n";
+	cout << "You can cancel the operation by typing -1. The operation will repeat until you type\"done\".\n";
+	for (int i = 0; i < atts.size(); ++i) { cout << i + 1 << ". " << atts[i] << endl; }
+	int num;
+	while (1) {
+		getline(cin, uin);
+		if (uin == "-1") { return; }
+		if (uin == "done") { break; }
+		try { num = stoi(uin); }
+		catch (...) { num = -1; }
+		if (num > 0 && num <= atts.size()) {
+			--num;
+			cout << "Please enter new value for " + atts[num] << endl;
+			getline(cin, uin);
+			if (uin == "-1") { return; }
+			update += atts[num] + " = '" + uin + "', ";
+		}
+		else { cout << "Invalid choice. Please try again.\n"; }
+		cout << "Select a new attribute to update.\n";
+	}
+	if (update.length() == 0) { cout << "No updates were queued. Aborting operation.\n"; return; }
+	// Remove leftover ", "
+	update.pop_back();
+	update.pop_back();
+	query = "UPDATE " + what;
+	query += " SET " + update;
+	query += " WHERE " + pk + " = " + pkin;
+	cout << "Updating table " + what + ".\n";
+	Finalize(db, query);
+}
+
+void DeleteTableEntry(sqlite3*, int d)
+{
+	string what, pkin, pk; // what = table being edited, pkin = user's selected id, pk = the primary key of the table being edited
+	vector<string> pks; // collection of available primary key values
+	switch (d) {
+	case 1: // Store
+		what = "STORE";
+		pk = "STORE_ID";
+		break;
+	case 2: // Employee
+		what = "EMPLOYEE";
+		pk = "EMPLOYEE_ID";
+		break;
+	case 3: // Customer
+		what = "CUSTOMER";
+		pk = "CUSTOMER_ID";
+		break;
+	case 4: // Product
+		what = "PRODUCT";
+		pk = "PRODUCT_ID";
+		break;
+	case 5: // Music
+		what = "MUSIC";
+		pk = "PRODUCT_ID";
+		break;
+	case 6: // Instrument
+		what = "INSTRUMENT";
+		pk = "PRODUCT_ID";
+		break;
+	case 7: // Sales
+		what = "SALES";
+		pk = "ORDER_ID";
+		break;
+	case 8: // Rentals
+		what = "RENTALS";
+		pk = "RENTAL_ID";
+		break;
+	case 9: // Services
+		what = "SERVICES";
+		pk = "SERVICE_ID";
+		break;
+	}
 }
 
 sqlite3_stmt* RunQuery(sqlite3* db, string q) {
@@ -309,6 +602,18 @@ sqlite3_stmt* RunQuery(sqlite3* db, string q) {
 		return nullptr;
 	}
 	else { return s; }
+}
+
+vector<string> MakeVector(sqlite3* db, string& q, sqlite3_stmt* s) {
+	vector<string> v;
+	if (s != nullptr) {
+		while (sqlite3_step(s) == SQLITE_ROW) {
+			v.push_back(reinterpret_cast<const char*>(sqlite3_column_text(s, 0)));
+		}
+	}
+	sqlite3_reset(s);
+	q.clear();
+	return v;
 }
 
 bool Finalize(sqlite3* db, string q) {
